d40c227465553743a41744335986607ad6bf47a1
Alexander Bodalevsky <abodalevsky@lohika.com>
2016-07-12 14:43:57 +0300

    add async helper react-thunk

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, compose(
+    applyMiddleware(thunkMiddleware)
+  ));
 }
